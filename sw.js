03d4335d24f8aef73f6955e9aacf4cdb65447957
nexocuba <nexocuba2026@gmail.com>
2026-01-20 06:53:17 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,23 @@
-const CACHE_NAME = 'nexo-cache-v1';
+const CACHE_NAME = "nexo-cache-v1";
+
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/servicios.html',
-  '/tienda.html',
-  '/ofertas.html',
-  '/quienes.html',
-  '/contacto.html',
-  '/style.css',   // si tienes CSS separado
-  // Agrega aquí las imágenes y otros archivos necesarios
-  '/images/servicio1.jpg',
-  '/images/servicio2.jpg'
+  "./",
+  "./index.html",
+  "./servicios.html",
+  "./tienda.html",
+  "./ofertas.html",
+  "./quienes.html",
+  "./contacto.html"
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
