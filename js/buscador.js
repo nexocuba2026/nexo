c247b843ec2c77a9f43ddf3d4e4e4e71a786165b
nexocuba <nexocuba2026@gmail.com>
2026-01-22 19:15:49 -0600

    Update buscador.js

--- js/buscador.js
+++ js/buscador.js
@@ -1,21 +1,50 @@
 // LISTA DE CONTENIDOS BUSCABLES
 const contenidos = [
-  { nombre: "Servicios de Marketing", url: "servicios.html" },
-  { nombre: "Diseño Gráfico", url: "servicios.html" },
-  { nombre: "Diseño de Interiores", url: "servicios.html" },
+  { nombre: "Construcción", url: "servicios.html" },
+  { nombre: "Diseño y construcción", url: "servicios.html" },
+  { nombre: "Transporte de carga y personal", url: "servicios.html" },
   { nombre: "Publicidad Digital", url: "servicios.html" },
+{ nombre: "Arquitectura Moderna", url: "servicios.html" },
+{ nombre: "Servicios Gastronómicos", url: "servicios.html" },
+{ nombre: "Comida Mexicana", url: "servicios.html" },
+{ nombre: "Restaurante Come 2", url: "servicios.html" },
+ { nombre: "Transporte Fast", url: "servicios.html" }, 
+{ nombre: "Fast", url: "servicios.html" },
+{ nombre: "Come 2", url: "servicios.html" },
+  { nombre: "X", url: "servicios.html" },
 
-  { nombre: "Tienda X", url: "tienda.html" },
-  { nombre: "Producto A", url: "tienda.html" },
-  { nombre: "Producto B", url: "tienda.html" },
+  { nombre: "Tienda A y B", url: "tienda.html" },
+  { nombre: "Cemento Portland", url: "tienda.html" },
+  { nombre: "Ladrillo rojo", url: "tienda.html" },
+{ nombre: "Olla", url: "tienda.html" },
+{ nombre: "Fogón Infrarrojo", url: "tienda.html" },
+{ nombre: "Productos", url: "tienda.html" },
+  { nombre: "Tiendas Disponibles", url: "tienda.html" },
+{ nombre: "Materiales de Construcción", url: "tienda.html" },
+  { nombre: "Tienda M", url: "tienda.html" },
+  { nombre: "Equipos electrodómesticos", url: "tienda.html" },
 
+  
+  
   { nombre: "Ofertas Plus", url: "ofertas.html" },
   { nombre: "Logotipos", url: "ofertas.html" },
   { nombre: "Tarjetas de Presentación", url: "ofertas.html" },
   { nombre: "Manual de Identidad", url: "ofertas.html" },
+  { nombre: "Flayers", url: "ofertas.html" },
+  { nombre: "Diseño de Interiores", url: "ofertas.html" },
+  { nombre: "Diseño Gráfico", url: "ofertas.html" },
+  { nombre: "Souvenirs", url: "ofertas.html" },
+  
 
   { nombre: "Quiénes Somos", url: "quienes.html" },
+    { nombre: "Nexo", url: "quienes.html" },
   { nombre: "Contactos", url: "contactos.html" }
+   { nombre: "whatsapp", url: "contactos.html" }
+ { nombre: "Correo", url: "contactos.html" }
+ { nombre: "Instagram", url: "contactos.html" }
+ { nombre: "Facebook", url: "contactos.html" }
+ { nombre: "Youtube", url: "contactos.html" }
+ { nombre: "Redes Sociales", url: "contactos.html" }
 ];
 
 const input = document.getElementById("buscador");
